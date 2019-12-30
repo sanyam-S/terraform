8ff2dba54469bae6872938f2ae7755d028d94c62
sanyam-S <49986897+sanyam-S@users.noreply.github.com>
2019-12-30 15:58:38 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -12,5 +12,7 @@ resource "aws_instance" "mylinux" {
 resource "aws_key_pair"  "terraform_ec2_key" {
      key_name = "terraform_ec2_key"
      public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAcDP3HpKPc2RQJX5FrZPozUbWEqXs7u2B1Lht2LRzior4KT4nnOgtAjb1mt8trLbsj4G3lIaO4tR5gbT1t9HFoQksOcYqdDjeKibm81ePlCg4LphlxYrp8zOwAjXVTR9L5Oq2ltnfDtTEo1eIlw5sDTqJWBVi6lxQrZNyB3AGz5j94JlMYyG6v0BbIxblrRDrQ23RdKpRpV2lr0SxltqE2ywf6jkGMjb+h1OBvbqgQevnfdhY0NQSgaA43TPh1ayz1sgBbGThMBVG7ZZYA9hGkpXZWw65nYjG/E1saNxuRhOcn9TdeMW9dC4PJAhiMhqNEx2LDBVWw5jWhmkkZ2Rwn root@ip-172-31-89-190.ec2.internal"
-}
+      //  public_key = "${file("terraform_ec2_key.pub")}"
+    }
+
 
